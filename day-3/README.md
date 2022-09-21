5b16962823d1fcbf3967ae776a3b8077b07cfe84
IqbalLx <iqbal19600@gmail.com>
2022-09-21 20:29:18 +0700

    feat: add day-3

---
+++ day-3/README.md
@@ -0,0 +1,5 @@
+# Go Middleware
+
+Apologize for the late submission. Actually here my submission for day 6, there I also implement the Log and JWT middleware.
+
+Cheers
---
+++ day-3/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/IqbalLx/alterra-agmc/utils"
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "remote_ip=${remote_ip}, method=${method}, uri=${uri}, status=${status}, latency=${latency_human}\n",
+	}))
+
+	e.Validator = (utils.NewRequestValidator(validator.New()))
+
+	v1 := e.Group("/v1")
+	InitializeServer(v1)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
